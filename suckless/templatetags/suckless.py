33db53d535be69e1e5f0232718b20a935e1838bc
starenka <starenka0@gmail.com>
2016-03-06 19:23:55 +0100

    typo

--- suckless/templatetags/suckless.py
+++ suckless/templatetags/suckless.py
@@ -11,7 +11,7 @@ def pdb(x):
     if settings.DEBUG:
         import pdb
         pdb.set_trace()
-        return x  # <---- this is your filtered variable. Have fun'''
+        return x  # <---- this is your filtered variable. Have fun!
     return x
 
 
@@ -20,5 +20,5 @@ def ipdb(x):
     if settings.DEBUG:
         import ipdb
         ipdb.set_trace()
-        return x  # <---- this is your filtered variable. Have fun'''
+        return x  # <---- this is your filtered variable. Have fun!
     return x
